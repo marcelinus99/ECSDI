c972a1a5aeda88e57b744c27dad0dc8bed2557db
Marc Gonzalez Moratona <marc.gonzalez.moratona@estudiantat.upc.edu>
2021-05-05 12:26:11 +0200

    array

--- app.py
+++ app.py
@@ -1,21 +1,31 @@
 from flask import Flask, request
+import requests
 
 app = Flask(__name__)
 
 url = 'http://127.0.0.1:5000/'
 
+array = ["pepito","a"]
+
 
 @app.route('/')
 def hello():
+    numeros = {'x': 'olaktal'}
+    requests.post(url + '/agente', params=numeros)
     return "Hello, World!"
 
 
 @app.route('/agente', methods=['GET', 'POST'])
 def peticion_objeto():
     if request.method == 'GET':
-        return 'Hola, soy un agente'
+        #peticion = {'x': 22, 'y': 65}
+        #r = requests.get(url + '/sumador', params=peticion)
+        print(array)
+        return array[5]
     else:
-        return 'POST recibido'
+        x = request.args['x']
+        array.insert(2, x)
+        return 'posteado'
 
 
 @app.route('/sumador')
@@ -27,5 +37,3 @@ def sumador():
 
 if __name__ == '__main__':
     app.run()
-
-
